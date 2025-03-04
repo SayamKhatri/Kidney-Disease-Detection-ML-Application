885721e15fdc0b451c6d11fdb3051ffdf9fdbfc2
SayamKhatri <khatri.say@northeastern.edu>
2025-03-04 18:03:30 -0500

    prepared base model

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-from src.pipeline.stage_01_data_ingestion import *
+from src.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.pipeline.stage_02_base_model import PrepareBaseModelTrainingPipeline
 from src import logger
 
-if __name__ == '__main__':
-    try:
-        logger.info(f'{STAGE_NAME} started')
-        obj = DataIngestionTrainingPipeline()
-        obj.main()
-        logger.info(f'{STAGE_NAME} completed')
-    except Exception as e:
-        logger.exception(e)
-        raise e
+
+STAGE_NAME = "DATA INGESTION"
+try:
+    logger.info(f'{STAGE_NAME} started')
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f'{STAGE_NAME} completed')
+except Exception as e:
+    logger.exception(e)
+    raise e
+    
+
+
+STAGE_NAME = "PREPARE BASE MODEL"
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
