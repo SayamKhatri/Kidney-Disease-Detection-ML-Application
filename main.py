471349fc43c987b068f81302a3b10d7088dd672e
SayamKhatri <khatri.say@northeastern.edu>
2025-03-05 11:19:36 -0500

    Model Training Added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from mypackage.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mypackage.pipeline.stage_02_base_model import PrepareBaseModelTrainingPipeline
+from mypackage.pipeline.stage_03_model_training import ModelTrainingPipeline
 from mypackage import logger
 
 
@@ -20,6 +21,18 @@ try:
     logger.info(f"*******************")
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "TRAIN MODEL"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
---
+++ src/mypackage/pipeline/stage_03_model_training.py
@@ -0,0 +1,32 @@
+from mypackage.config.configuration import ConfigurationManager
+from mypackage.components.model_training import Training
+from mypackage import logger
+
+
+STAGE_NAME = "Training the Model"
+
+
+class ModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        training_config = config.get_training_config()
+        training = Training(config=training_config)
+        training.get_base_model()
+        training.train_valid_generator()
+        training.train()
+
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+        
